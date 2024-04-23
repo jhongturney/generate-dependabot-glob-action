2090c6836451ac5e0105c520db07aaef74b52a67
jhongturney <jhongturney@github.com>
2024-04-23 13:48:54 -0700

    let's use the function

--- dist/index.js
+++ dist/index.js
@@ -38983,7 +38983,8 @@ const core = __nccwpck_require__(2186)
 const yaml = __nccwpck_require__(1917)
 const fs = (__nccwpck_require__(7147).promises)
 const util = __nccwpck_require__(3837)
-const glob = __nccwpck_require__(8211)
+const globModule = __nccwpck_require__(8211)
+const glob = util.promisify(globModule.glob)
 const path = __nccwpck_require__(1017)
 
 const actionOpts = {
